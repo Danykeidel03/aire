a994fad25fe2a4550c089183a59439735c5a4baf
Daniel <supertuxcar@gmail.com>
2021-11-17 12:11:41 +0100

    incremento

--- aireAcondicionado.java
+++ aireAcondicionado.java
@@ -1,15 +1,24 @@
-public class aireAcondicionado {
+public class AireAcondicionado {
 
     double temperatura;
+    double max;
+    double min;
+    double incremento;
 
-    public aireAcondicionado() {
+    public AireAcondicionado(double newMax, double newMin) {
         temperatura = 15;
+        incremento = 5.0;
+        max = newMax;
+        min = newMin;
     }
 
     /**
      * subir temperatura
      */
     public void subirTemperatura() {
+        if (temperatura == max) {
+            temperatura = max; 
+        }
         temperatura = temperatura + 5;
     }
     
@@ -17,7 +26,17 @@ public class aireAcondicionado {
      * bajar temperatura
      */
     public void bajarTemperatura() {
+        if (temperatura == min) {
+            temperatura = min; 
+        }
         temperatura = temperatura - 5;
+    }
+    
+    /**
+     * incremento
+     */
+    public void setIncremento( double cambioIncremento ) {
+        incremento = cambioIncremento;
     }
     
     /**
